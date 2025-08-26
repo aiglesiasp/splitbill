4167605345c5e8c2b72f06d5b3044d3152d11f6f
aiglesiasp <iglesiaspubillaitor@gmail.com>
2025-08-26 19:38:42 +0200

    Create Repository

---
+++ app/src/main/java/io/devexpert/splitbill/data/TicketRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package io.devexpert.splitbill.data
+
+import android.graphics.Bitmap
+import io.devexpert.splitbill.TicketData
+
+class TicketRepositoryImpl(private val ticketDataSource: TicketDataSource): TicketRepository {
+
+    private var _ticketData: TicketData? = null
+
+    override suspend fun processTicket(image: Bitmap): TicketData {
+        val result = ticketDataSource.processTicket(image)
+        _ticketData = result
+        return result
+    }
+}

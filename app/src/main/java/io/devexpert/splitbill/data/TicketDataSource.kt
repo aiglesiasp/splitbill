4846cc3b8ede6c9622615a4aa93dcf89aa51623e
aiglesiasp <iglesiaspubillaitor@gmail.com>
2025-08-26 19:28:17 +0200

    Create Data Source Interface

---
+++ app/src/main/java/io/devexpert/splitbill/data/TicketDataSource.kt
@@ -0,0 +1,8 @@
+package io.devexpert.splitbill.data
+
+import android.graphics.Bitmap
+import io.devexpert.splitbill.TicketData
+
+interface TicketDataSource {
+    suspend fun processTicket(image: Bitmap): TicketData
+}
